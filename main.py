44ee6fbf1c4892d584e8077a5a67e015000e1816
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-06 23:26:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# How many seconds are in a year
+daysyear = int(input("How many days are in this year?"))
+if daysyear == 365:
+  sec_y = 60 * 60 * 24 * daysyear
+  print("Number of seconds in a year are",sec_y)
+else:
+  sec_y = (60 * 60 * 24 * daysyear) + 60 * 60 * 24
+  print("Number of seconds in a leap year are",sec_y)
